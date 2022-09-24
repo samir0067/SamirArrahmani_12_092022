38c66298293d2fe01229badd78d52a1655514d58
samir0067 <arrahmani.samir@gmail.com>
2022-09-24 11:56:24 +0200

    wip find dependencies

---
+++ src/navigation/Routing.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import NotFound from "pages/NotFound"
+import {Route, Routes, useLocation} from "react-router-dom"
+
+function Routing(){
+  const location = useLocation()
+
+  return (
+    <Routes key={location.pathname} location={location}>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+  )
+}
+
+export default Routing
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,25 @@
+import React, {FC, Fragment} from "react"
+import {Link} from "react-router-dom"
+import "./notFound.css"
+
+const NotFound: FC = () => {
+  const content = {
+    title: "404",
+    message: "Oups! La page que vous demandez n'existe pas.",
+    link: "Retourner sur la page d’accueil"
+  }
+
+  return (
+    <Fragment>
+      <main>
+        <h1>{content.title}</h1>
+        <p>{content.message}</p>
+        <Link className="linkRollback" to="/">
+          {content.link}
+        </Link>
+      </main>
+    </Fragment>
+  )
+}
+
+export default NotFound
