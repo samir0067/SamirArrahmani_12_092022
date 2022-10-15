1edb1c363c17b849c067823ef2e12a68dc3abd47
samir0067 <arrahmani.samir@gmail.com>
2022-10-15 17:31:59 +0200

    wip ajout du radar de formation

---
+++ src/components/Performance.tsx
@@ -0,0 +1,42 @@
+import React, { FC, Fragment } from "react";
+import { PolarAngleAxis, PolarGrid, Radar, RadarChart, ResponsiveContainer } from "recharts";
+import { SessionsPerformance } from "utils/types";
+
+type PerformanceProps = {
+  performance: SessionsPerformance[];
+};
+
+const Performance: FC<PerformanceProps> = ({ performance }: PerformanceProps) => {
+  const kindFr = {
+    1: "Intensité",
+    2: "Vitesse",
+    3: "Force",
+    4: "Endurance",
+    5: "Énergie",
+    6: "Cardio",
+  };
+
+  const formatKind = (indexKind: number) => {
+    return kindFr[indexKind as keyof typeof kindFr];
+  };
+
+  return (
+    <Fragment>
+      <ResponsiveContainer width="100%" height="100%">
+        <RadarChart className="radarChart" cx="50%" cy="50%" outerRadius="65%" data={performance}>
+          <PolarGrid className="radarChart_polarGrid" radialLines={false} />
+          <PolarAngleAxis
+            dataKey="kind"
+            stroke="#ffffff"
+            tickLine={false}
+            axisLine={false}
+            tickFormatter={formatKind}
+          />
+          <Radar dataKey="value" name="Performance" fill="rgba(255, 1, 1, 0.7)" />
+        </RadarChart>
+      </ResponsiveContainer>
+    </Fragment>
+  );
+};
+
+export default Performance;
